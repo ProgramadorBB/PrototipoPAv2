7d483120edfca62dd3ab821abc4a631130dff393
BYRON MATIAS BRAVO GARCIA <byron.bravo@inacapmail.cl>
2018-12-06 18:35:05 -0300

    Se añadio carpeta Modelo con la clasa y tabla Usuario, Se añadio carpeta Conexiones con la conexión del Usuario y los metodos añadir nuevo usuario y mostrar todos los usuarios

---
+++ PrototipoPAv2/PrototipoPAv2/PrototipoPAv2/Conexiones/UsuarioRepository.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using PrototipoPAv2.Modelo;// llamamos a la carpeta modelo para poder usar la clase usuario
+
+namespace PrototipoPAv2.Conexiones
+{
+    public class UsuarioRepository
+    {
+        private SQLiteConnection con;
+        private static UsuarioRepository instancia;
+        public static UsuarioRepository Instancia
+        {
+            get
+            {
+                if (instancia == null)
+                    throw new Exception("Debe llamar al Inicializador");
+
+                return Instancia;
+            }
+                
+        }
+
+        public static void Inicializador(String filename)
+        {
+            if (filename == null)
+                throw new ArgumentNullException();
+            if (instancia != null)
+                instancia.con.Close();
+
+            instancia = new UsuarioRepository(filename);
+        }
+        private UsuarioRepository(String dbPath)
+        {
+            con = new SQLiteConnection(dbPath);
+
+            con.CreateTable<Usuario>();
+        }
+
+        public string EstadoMensaje;
+
+        public void AddNuevoUsuario(string email, string contraseña, 
+                                    string nombre, string apellido,
+                                    string color, string tipo,
+                                    string estado)
+        {
+            int result = 0;
+            try
+            {
+                result = con.Insert(new Usuario() {
+                                     Email = email,
+                                     Contraseña = contraseña,
+                                     Nombre = nombre,
+                                     Apellido = apellido,
+                                     Color = color,
+                                     Tipo = tipo,
+                                     Estado = estado
+                                    });
+                EstadoMensaje = string.Format("{0} Nuevo usuario añadido", result);
+            } catch(Exception e)
+            {
+                EstadoMensaje = e.Message;
+            }
+        }
+
+        public IEnumerable<Usuario> GetAllUsuarios()
+        {
+            try
+            {
+                return con.Table<Usuario>();
+            } catch(Exception e)
+            {
+                EstadoMensaje = e.Message;
+            }
+            return Enumerable.Empty<Usuario>();
+        }
+
+    }
+}
---
+++ PrototipoPAv2/PrototipoPAv2/PrototipoPAv2/Modelo/Usuario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace PrototipoPAv2.Modelo
+{
+    [Table("Usuarios")]
+    public class Usuario
+    {
+        [PrimaryKey, AutoIncrement, Column("id")]
+        public int Id { get; set; }
+        [MaxLength(100),Unique]
+        public string Email { get; set; }
+        [MaxLength(100)]
+        public string Contraseña { get; set; }
+        [MaxLength(100)]
+        public string Nombre { get; set; }
+        [MaxLength(100)]
+        public string Apellido { get; set; }
+        //nose si va algo acá para el int de EDAD
+        // public int Edad { get; set; } no sabemos si es relevante
+        [MaxLength(25)]
+        public string Color { get; set; }
+        [MaxLength(25)]
+        public string Tipo { get; set; }
+        [MaxLength(25)]
+        public string Estado { get; set; }
+
+
+    }
+}
